2c9ec1449bc6cb047800837330881a14890d99c7
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2018-01-24 13:23:14 -0600

    added collision detection

--- server/public/scripts/classes/Rect.js
+++ server/public/scripts/classes/Rect.js
@@ -1,20 +1,27 @@
-import {Vec2} from './math.js';
+import { Vec2 } from './math.js';
 export default class Rect {
-  constructor(context,x = 0, y = 0,width = 100,height = 100) {
+  constructor(context, x = 0, y = 0, width = 100, height = 100, velX = 0, velY = 0) {
     this.size = new Vec2;
     this.pos = new Vec2;
+    this.velocity = new Vec2;
     this.context = context;
-    this.setSize(width,height);
-    this.setPosition(x,y);
+    this.setSize(width, height);
+    this.setPosition(x, y);
+    this.setVelocity(velX, velY);
+    console.log(context);
     this.render();
   }
-  setSize (w,h) {
+  setSize(w, h) {
     this.size.x = w;
     this.size.y = h;
   }
-  setPosition(x,y) {
+  setPosition(x, y) {
     this.pos.x = x;
     this.pos.y = y;
+  }
+  setVelocity(x, y) {
+    this.velocity.x = x;
+    this.velocity.y = y;
   }
 
   clear() {
@@ -22,7 +29,52 @@ export default class Rect {
   }
 
   render() {
-    this.context.strokeRect(this.pos.x,this.pos.y,this.size.x,this.size.y);
+    this.context.strokeRect(this.pos.x, this.pos.y, this.size.x, this.size.y);
+  }
+
+  update() {
+    let x = this.pos.x;
+    let x2 = x + this.size.x;
+    let vx = this.velocity.x;
+    let y = this.pos.y;
+    let y2 = y + this.size.y;
+    let vy = this.velocity.y;
+    let w = this.context.canvas.width;
+    let h = this.context.canvas.height;
+
+    this.setPosition(x + vx,y);
+    x = x + vx;
+    if (vx > 0) {
+      if (x2 > w) {
+        console.log('collision x+');
+        this.setPosition(w - this.size.x,y);
+        this.setVelocity(-vx,vy);
+      }
+    }
+    if (vx < 0) {
+      if (x < 0) {
+        console.log('collision x-');
+        this.setPosition(0,y);
+        this.setVelocity(-vx,vy);
+      }
+    }
+    this.setPosition(x,y+vy);
+    y = y + vy;
+    if (vy > 0) {
+      if (y2 > h) {
+        console.log('collision y+');
+        this.setPosition(x,h - this.size.y);
+        this.setVelocity(vx,-vy);
+      }
+    }
+    if (vy < 0) {
+      if (y < 0) {
+        console.log('collision y-');
+        this.setPosition(x,0);
+        this.setVelocity(vx,-vy);
+      }
+    }
+    this.render();
   }
 
 }
@@ -33,28 +85,42 @@ export function clearRect(array) {
   })
 }
 
-export function connectRect(array,context) {
+export function updateRect(array) {
+  array.forEach((rect) => {
+    rect.update();
+  })
+}
+
+export function connectRect(array, context) {
   let c = context;
-  array.forEach((rect,i,arr) => {
-    arr.forEach((otherRect,j) => {
-      c.beginPath();
-      c.moveTo(rect.pos.x,rect.pos.y);
-      c.lineTo(otherRect.pos.x,otherRect.pos.y);
-      c.stroke();
-      c.beginPath();
-      c.moveTo(rect.pos.x + rect.size.x,rect.pos.y);
-      c.lineTo(otherRect.pos.x + otherRect.size.x,otherRect.pos.y);
-      c.stroke();
-      c.beginPath();
-      c.moveTo(rect.pos.x,rect.pos.y + rect.size.y);
-      c.lineTo(otherRect.pos.x,otherRect.pos.y + otherRect.size.y);
-      c.stroke();
-      c.beginPath();
-      c.moveTo(rect.pos.x + rect.size.x,rect.pos.y + rect.size.y);
-      c.lineTo(otherRect.pos.x + otherRect.size.x, otherRect.pos.y + otherRect.size.y);
-      c.stroke();
-
-
+  array.forEach((rect, i, arr) => {
+    arr.forEach((otherRect, j) => {
+      if (i !== j) {
+        let x = rect.pos.x;
+        let y = rect.pos.y;
+        let xs = rect.size.x;
+        let ys = rect.size.y;
+        let x2 = otherRect.pos.x
+        let y2 = otherRect.pos.y
+        let xs2 = otherRect.size.x
+        let ys2 = otherRect.size.y
+        c.beginPath();
+        c.moveTo(x,y);
+        c.lineTo(x2,y2);
+        c.stroke();
+        c.beginPath();
+        c.moveTo(x + xs, y);
+        c.lineTo(x2 + xs2,y2);
+        c.stroke();
+        c.beginPath();
+        c.moveTo(x, y + ys);
+        c.lineTo(x2, y2 + ys2);
+        c.stroke();
+        c.beginPath();
+        c.moveTo(x + xs, y + ys);
+        c.lineTo(x2 + xs2, y2 + ys2);
+        c.stroke();
+      }
     })
   })
 }
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,4 @@
-import Rect, {clearRect, connectRect} from './classes/Rect.js';
+import Rect, {clearRect, connectRect, updateRect} from './classes/Rect.js';
 window.onload = () => {
   let canvas = document.getElementById('GoL');
   
@@ -9,22 +9,16 @@ window.onload = () => {
 
   let ctx = canvas.getContext('2d');
 
-  let rect1 = new Rect(ctx);
-  let rect2 = new Rect(ctx, 500,100,100,100);
-  let rect3 = new Rect(ctx, 300,100,10,10);
+  let rect1 = new Rect(ctx,100,100,100,100,3,2);
+  let rect2 = new Rect(ctx, 500,100,200,200,-4,1);
+  let rect3 = new Rect(ctx, 300,100,10,10,.5,.7);
   let rects = [rect1,rect2,rect3];
   setInterval(() => {
     
     ctx.clearRect(0,0,canvas.width, canvas.height);
-    rect1.setPosition(rect1.pos.x + 1,rect1.pos.y + 1);
-    rect1.setSize(rect1.size.x + 1,rect1.size.y + 1);
-    rect1.render();
-    rect2.setPosition(rect2.pos.x - 1,rect2.pos.y + 1);
-    // rect2.setSize(rect2.size.x + 1,rect2.size.y + 1);
-    rect2.render();
-    rect3.render();
+    updateRect(rects);
     connectRect(rects,ctx);
-  }, 10);
+  }, 1);
 
 }
 
